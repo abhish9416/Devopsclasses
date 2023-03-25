942276a44f30129f186ba27a466603b1cf0273a9
abhishek <abhishek9416@outlook.com>
2023-03-25 11:07:18 +0530

    added new steps for azure subnet

--- azure_vnet_terraform/input.tf
+++ azure_vnet_terraform/input.tf
@@ -4,8 +4,22 @@ variable "location" {
   default     = "eastus"
 
 }
-variable "ntier_cidr_range" {
-  type        = list(string)
-  description = "Ntier Cidr Range"
-  default     = ["192.168.0.0/16"]
+# variable "ntier_cidr_ranges" {
+#   type        = list(string)
+#   description = "Ntier Cidr Range"
+#   default     = ["192.168.0.0/16"]
+# }
+# variable "web_range" {
+#   type = list(string)
+#   default = [ "192.168.0.0/24" ]
+# }
+variable "web_subnets" {
+  type = object({
+    ntier_cidr_range = list(string)
+    Web_name         = list(string)
+  })
+  default = {
+    Web_name = [ "app1" , "app2" ]
+    ntier_cidr_range = [ "192.168.0.0/16" ]
+  }
 }
--- azure_vnet_terraform/values.tfvars
+++ azure_vnet_terraform/values.tfvars
@@ -1,2 +1,5 @@
-location         = "eastus"
-ntier_cidr_range = ["10.10.0.0/16"]
+location = "eastus"
+web_subnets = {
+  Web_name = [ "app1" , "app2" , "web1" , "web2" ]
+  ntier_cidr_range = [ "10.10.0.0/16" ]
+}
