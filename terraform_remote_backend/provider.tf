fcf49b4b73e7bbcf830ef4862254a0c817143f74
abhishek <abhishek9416@outlook.com>
2023-03-30 21:12:19 +0530

    added terraform remote backend

---
+++ terraform_remote_backend/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+  backend "s3" {
+    bucket = "abhish0916terraforraform"
+    region = "us-west-2"
+    key = "classes/terraform_remote_backendtf"
+    dynamodb_table = "terraformlocking"
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+  # Configuration options
+}
