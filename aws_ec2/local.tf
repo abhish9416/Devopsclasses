aef479adb199675435e7a3fb3937ddf542516ef2
abhishek <abhishek9416@outlook.com>
2023-03-31 11:20:41 +0530

    removed security group

--- aws_ec2/local.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  route_cidr = "0.0.0.0/16"
-  tcp        = "tcp"
-  ssh_port   = 22
-  http_port  = 80
-}
--- aws_ec2/resource.tf
+++ aws_ec2/resource.tf
@@ -1,35 +1,9 @@
-resource "aws_security_group" "ec2_sg" {
-  name = "ec2_sg"
-  ingress {
-    from_port   = local.ssh_port
-    to_port     = local.ssh_port
-    cidr_blocks = [local.route_cidr]
-    protocol    = local.tcp
 
-  }
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    cidr_blocks = [local.route_cidr]
-    protocol    = local.tcp
-
-  }
-  tags = {
-    Name = "web"
-  }
-
-
-}
 resource "aws_instance" "app" {
   ami                         = var.ec2_details.ami_id
   associate_public_ip_address = true
-  security_groups = [aws_security_group.ec2_sg.name]
   instance_type = var.ec2_details.instance_type
   tags = {
     Name = "Ec2_app"
   }
-  depends_on = [
-    aws_security_group.ec2_sg
-  ]
-
 }
